bcd704dbba7f9cd9b741ca25c4c5aab8000aa789
Eisler Aguilar <eislerandres1@gmail.com>
2021-04-14 14:34:11 -0400

    Se puede editar los usuarios

--- ejercicio_2/app/controllers/users_controller.rb
+++ ejercicio_2/app/controllers/users_controller.rb
@@ -7,10 +7,22 @@ class UsersController < ApplicationController
     def create
         # Ingresa los valores del formulario en el nuevo item
         @user = User.create!(tipo: params[:user][:tipo], identificacion: params[:user][:identificacion], emision_id: params[:user][:emision_id], venc_id: params[:user][:venc_id], nombre: params[:user][:nombre], correo: params[:user][:correo], tlf_prin: params[:user][:tlf_prin], tlf_sec: params[:user][:tlf_sec])
-        render json: @user
+        redirect_to @user
     end
 
     def show
+        # Busca el usuario de acuerdo a su ID y lo muestra en el html de Show
         @user = User.find(params[:id])
     end
+
+    def edit
+        @user = User.find(params[:id])  
+    end
+
+    def update
+        @user = User.find(params[:id])
+        @user.update!(tipo: params[:user][:tipo], identificacion: params[:user][:identificacion], emision_id: params[:user][:emision_id], venc_id: params[:user][:venc_id], nombre: params[:user][:nombre], correo: params[:user][:correo], tlf_prin: params[:user][:tlf_prin], tlf_sec: params[:user][:tlf_sec])
+        redirect_to @user
+    end
+    
 end
