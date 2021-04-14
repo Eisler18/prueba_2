a2fa58fe7c0d37307454c82a67551f5df34c6723
Eisler Aguilar <eislerandres1@gmail.com>
2021-04-14 16:16:38 -0400

    Se agrego validacion del formato del correo

--- ejercicio_2/app/models/user.rb
+++ ejercicio_2/app/models/user.rb
@@ -9,4 +9,7 @@ class User < ApplicationRecord
     validates :tlf_prin, format: {with: /\A[0]/, message: "El tlf debe iniciar por '0'"}
     # Se valida que el tlf e identificacion sean unicamente numeros
     validates :tlf_prin, :identificacion, numericality: true
+    # Se valida el formato del correo, este formato se obtuvo de 'https://medium.com/@jgratereaux/validar-correos-electr%C3%B3nicos-con-expresiones-regulares-7914751b6018'
+    validates :correo, format: {with: /\A(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))\z/, 
+                                message: "Formato de correo invalido"}
 end
